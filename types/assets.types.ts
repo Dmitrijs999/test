8a3a44e6e4a4aca65edae3ae6d1d25b2098f3841
Dmitrijs999 <dmitrijs@minterest.com>
2024-06-13 12:26:50 +0300

    sdf

--- types/assets.types.ts
+++
@@ -1,9 +0,0 @@
-export enum AssetsKeys {
-  totalSupplyUSD = 'totalSupplyUSD',
-  yourTotalSupplyUSD = 'yourTotalSupplyUSD',
-  totalBorrowBalanceUSD = 'totalBorrowBalanceUSD',
-  yourTotalBorrowBalanceUSD = 'yourTotalBorrowBalanceUSD',
-  tvl = 'tvl',
-  netApy = 'netApy',
-  utilization = 'utilization',
-}
